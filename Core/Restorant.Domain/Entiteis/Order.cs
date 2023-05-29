a4f26bd06a18bbf1dce3ffd2023152cd3e1026bf
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-05-29 23:38:22 +0300

    Proje dosyası ekle.

---
+++ Core/Restorant.Domain/Entiteis/Order.cs
@@ -0,0 +1,19 @@
+﻿using Restorant.Domain.Entiteis.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restorant.Domain.Entiteis
+{
+    public class Order:BaseEntity
+    {
+        public double TotalPrice { get; set; }
+        public bool ActiveOrder { get; set; }
+        public ICollection<Customer> Customers { get; set; }
+        public ICollection<Food> Foods { get; set; }
+        public Table Table { get; set; }
+
+    }
+}
---
+++ Infrasturucture/Restorant.Persistence/Repositorys/_Order/OrderReadRepository.cs
@@ -0,0 +1,19 @@
+﻿using Restorant.Application.Repositorys;
+using Restorant.Application.Repositorys._Order;
+using Restorant.Domain.Entiteis;
+using Restorant.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restorant.Persistence.Repositorys._Order
+{
+    public class OrderReadRepository : ReadRepository<Order>, IOrderReadRepository
+    {
+        public OrderReadRepository(RestorantDbContext context) : base(context)
+        {
+        }
+    }
+}
